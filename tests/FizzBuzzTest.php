20ae1e8e89356c462eec6395c422a587122d1780
victorgonzalezs <35777328+victorgonzalezs@users.noreply.github.com>
2019-02-25 10:58:20 -0300

    Update FizzBuzzTest.php

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -8,7 +8,7 @@ use FizzBuzz\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
- 	public function testNormalInputSet() {
+ 	  public function testNormalInputSet() {
         $fizzBuzz = new FizzBuzz();
         $inputSet = 17;
         $expectedResults = '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17';
