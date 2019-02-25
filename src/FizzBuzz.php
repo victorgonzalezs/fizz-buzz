72bb8a0e13ff07e63b857690bc8a9e855190fd25
victorgonzalezs <35777328+victorgonzalezs@users.noreply.github.com>
2019-02-25 10:56:58 -0300

    Update FizzBuzz.php

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,5 +4,29 @@ namespace FizzBuzz;
 
 class FizzBuzz
 {
+	public function printFizzBuzz($num){
+		$count = 1;
+		$saida = "";
+		while($count<=17){
+			$div3 = $count%3 == 0 ? true: false;		
+			$div5 = $count%5 == 0 ? true: false;
+
+			if(!empty($saida)){
+				$saida .= ', ';
+			}
+			if($div3 && $div5){
+				$saida .= 'FizzBuzz';			
+			} elseif ($div3){
+				$saida .= 'Fizz';
+			} elseif($div5){
+				$saida .= 'Buzz';
+			} else {
+				$saida .= $count;
+			}
+			$count++;
+		}
+		return $saida;
+	}
+
 
 }
